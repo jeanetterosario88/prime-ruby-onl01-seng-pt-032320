95e768eb137500e74d91a7b48a08e041f7df7700
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 05:27:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,15 @@
-def prime?(n)
-  (2..n/2)
-  interger.none? |i| 
-  n % i == 0
+def prime?(interger)
+  if n<
+    return true
+    elsif interger==1
+      return true
+    elsif interger==-1
+      return true
+    elsif interger%2!=0  
+      return true
+    elsif interger*-1%2!=0
+      return true
+  else
+    return false
+  end
 end
