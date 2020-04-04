b4dbf69cd4eced5317d77d9bf423bdbbdf156ee0
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 06:32:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(integer)
   return true if integer==2
   return false if integer<2
-  (2..integer-1).each do |x| 
+  (2..(integer-1)).each do |x| 
     return false if (integer % x) == 0 
     return true if (integer % x) != 0 
   end
-end
