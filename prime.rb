1a4fe21ce97e59e6d10ac3a5bccbba7a7a1c2f98
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 05:15:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 def prime?(interger)
-  if interger==2
+  if n<=3
     return true
-    elsif interger==1
-      return true
-    elsif interger==-1
-      return true
-    elsif interger%2!=0  
-      return true
-    elsif interger*-1%2!=0
-      return true
-  else
-    return false
-  end
-end
+    
