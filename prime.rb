ab61273b36056af76ad087a727128588c94c8d5c
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 05:12:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(interger)
+  if interger==2
+    return true
+    elsif interger%2!=0  
+      return true
+    elsif interger*-1%2!=0
+      return true
+  else
+    return false
+  end
+end
