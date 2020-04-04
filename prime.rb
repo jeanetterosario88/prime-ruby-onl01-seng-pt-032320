0e9ed34072cea80cf15d9e622e92dd9e5f101ee9
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 06:15:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,5 @@
 def prime?(integer)
 interger.to_a=intergerasarray
 newrange<<(2..integerasarray-1)
-  if integerasarray<2
-    return false
-  elsif newrange.each do |x|
-    (integer % x) == 0
-    return false
-  else
-    return true
-  end
+newrange.each {|x| return false if (integer % x) == 0 }
 end
